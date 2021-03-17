a9d2b7e4452dca323385f66e4d56d5bd4b92fa41
dcharney <delaneycharney@gmail.com>
2021-03-17 21:25:43 +0100

    submit and form state working

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    
     //JSX
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" onChange={handleChange} name="name" defaultValue={name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" onChange={handleChange} name="email" defaultValue={email} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea onChange={handleChange} name="message" defaultValue={message} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
